ae4d4192e80af9ce972381229cee0f41b4b218f5
alex <rusticalex@yahoo.com>
2024-11-27 13:31:49 +0100

    update

--- src/paradict/deserializer/__init__.py
+++ src/paradict/deserializer/__init__.py
@@ -9,7 +9,8 @@ from paradict.deserializer.unpacker import Unpacker
 __all__ = ["decode", "read", "unpack", "load"]
 
 
-def decode(text, type_ref=None, receiver=None, obj_builder=None):
+def decode(text, type_ref=None, receiver=None, obj_builder=None,
+           root_dir=None):
     """
     Convert some textual Paradict data into a Python dictionary
 
@@ -20,16 +21,18 @@ def decode(text, type_ref=None, receiver=None, obj_builder=None):
     This callback function accepts the Decoder instance as argument
     - obj_builder: function that accepts a paradict.box.Obj container and
     returns a fresh new Python object
+    - root_dir: root directory in which the attachments dir is supposed to be
 
     [return]
     Return the newly built Python object
     """
     decoder = Decoder(type_ref=type_ref, receiver=receiver,
-                      obj_builder=obj_builder)
+                      obj_builder=obj_builder,
+                      root_dir=root_dir)
     decoder.feed(text)
     if decoder.queue.buffer:
         decoder.feed("\n")
-    decoder.feed("===\n")
+    decoder.feed("===\n")  # it's important to send an explicit end of stream
     return decoder.data
 
 
--- src/paradict/serializer/__init__.py
+++ src/paradict/serializer/__init__.py
@@ -10,7 +10,8 @@ from paradict import const
 __all__ = ["encode", "write", "pack", "dump"]
 
 
-def encode(data, *, mode=const.DATA_MODE, type_ref=None):
+def encode(data, *, mode=const.DATA_MODE, type_ref=None,
+           bin_to_text=True, root_dir=None, attachments_dir="attachments"):
     """
     Serialize a Python dict object with the Paradict binary format
 
@@ -19,11 +20,16 @@ def encode(data, *, mode=const.DATA_MODE, type_ref=None):
     - mode: either const.DATA_MODE or const.CONFIG_MODE. Defaults to DATA_MODE.
     - type_ref: optional TypeRef object
     - bin_to_text: boolean to tell whether bin data should be converted into text or not
+    - root_dir: root directory in which the attachments dir is supposed to be
+    - attachments_dir: attachments directory. This is a path that is relative to the root dir.
+     Note that relative paths should use a slash as separator.
 
     [return]
     Return a string in the Paradict text format
     """
-    encoder = Encoder(mode=mode, type_ref=type_ref)
+    encoder = Encoder(mode=mode, type_ref=type_ref,
+                      bin_to_text=bin_to_text, root_dir=root_dir,
+                      attachments_dir=attachments_dir)
     lines = list()
     for r in encoder.encode(data):
         lines.append(r)
